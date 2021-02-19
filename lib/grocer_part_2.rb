f9ecbd8b660168d84d79926168eb82055aac46d7
Yuri Ng <yuringuyen312@gmail.com>
2021-02-19 20:58:07 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,25 +1,69 @@
 require_relative './part_1_solution.rb'
-
+require"pry"
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+
+    coupons.map do |coupon|
+      cart_item = find_item_by_name_in_collection(coupon[:item], cart)
+      couponed_item_in_basket = "#{coupon[:item]} W/COUPON"
+      if cart_item !=nil && cart_item[:count] >= coupon[:num]
+        cart << {:item => couponed_item_in_basket,
+        :price => coupon[:cost] / coupon[:num],
+      :clearance =>cart_item[:clearance],
+    :count => coupon[:num] }
+    cart_item[:count] -= coupon[:num]
+      end
+    end
+  p cart
 end
+apply_coupons([
+  {:item => "AVOCADO", :price => 3.00, :clearance => true, :count => 3},
+  {:item => "KALE",    :price => 3.00, :clearance => false, :count => 1}
+],
+[
+  {:item => "AVOCADO", :num => 2, :cost => 5.00},
+  {:item => "BEER", :num => 2, :cost => 20.00},
+  {:item => "CHEESE", :num => 3, :cost => 15.00}
+])
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+#new_cart=[]
+cart.map do |cart_item|
+  if cart_item[:clearance]
+    cart_item[:price] = (cart_item[:price]*0.8).round(2)
+    #binding.pry
+  end
 end
+p cart
+end
+apply_clearance([
+{:item => "PEANUT BUTTER", :price => 3.00, :clearance => true,  :count => 2},
+{:item => "KALE", :price => 3.00, :clearance => false, :count => 3},
+{:item => "SOY MILK", :price => 4.50, :clearance => true,  :count => 1}
+])
+
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
   # This method should call
   # * consolidate_cart
   # * apply_coupons
   # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+consolidated_cart = consolidate_cart(cart)
+couponed_cart = apply_coupons(consolidated_cart,coupons)
+final_cart =apply_clearance(couponed_cart)
+total = 0
+final_cart.map do |item|
+  total += item[:price] * item[:count]
 end
+    total = (total * 0.9).round(2) if total > 100
+  p total
+
+end
+checkout([
+  {:item => "AVOCADO", :price => 3.00, :clearance => true, :count => 3},
+  {:item => "KALE",    :price => 3.00, :clearance => false, :count => 1}
+],
+[
+  {:item => "AVOCADO", :num => 2, :cost => 5.00},
+  {:item => "BEER", :num => 2, :cost => 20.00},
+  {:item => "CHEESE", :num => 3, :cost => 15.00}
+])
--- lib/part_1_solution.rb
+++ lib/part_1_solution.rb
@@ -1,8 +1,10 @@
+
+
 def find_item_by_name_in_collection(name, collection)
   index = 0
 
   collection.each do |grocery_item|
-    return grocery_item if grocery_item[:item] === name 
+    return grocery_item if grocery_item[:item] === name
     index += 1
   end
 
@@ -13,7 +15,7 @@ end
 def consolidate_cart(cart)
   index = 0
   new_cart = []
-  
+
   cart.each do |grocery_item|
     current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
     if current_item
