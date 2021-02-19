ce0fcccb5a214e11bfd6fde843e59d11bb80be77
Yuri Ng <yuringuyen312@gmail.com>
2021-02-19 20:47:25 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -53,7 +53,6 @@ final_cart =apply_clearance(couponed_cart)
 total = 0
 final_cart.map do |item|
   total = item[:price] * item[:count]
-
     total = (total * 0.9).round(2) if total > 100
   p total
 end
